229b5968e7fc36ab19cb8cd822e1cb75ae536102
John Hao <johnhaoallwood@gmail.com>
2025-06-10 19:34:03 -0400

    add taskfiles for examples

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,52 +1,34 @@
-#include <algorithm>
-#include <string_view>
-#include <system_error>
+#include <string>
+#include <ystdlib/error_handling/ErrorCode.hpp>
+#include <cstdint>
+#include <iostream>
 
 #include <ystdlib/error_handling/ErrorCode.hpp>
 
-#include "constants.hpp"
-#include "types.hpp"
+enum class BinaryErrorCodeEnum : uint8_t {
+    Success = 0,
+    Failure
+};
 
-#include <cassert>
-#include <iostream>
+using BinaryErrorCode = ystdlib::error_handling::ErrorCode<BinaryErrorCodeEnum>;
 
-using namespace ystdlib::error_handling::test;
+using BinaryErrorCategory = ystdlib::error_handling::ErrorCategory<BinaryErrorCodeEnum>;
+
+constexpr std::string_view cBinaryTestErrorCategoryName{"Binary Error Code"};
+
+template <>
+auto BinaryErrorCategory::name() const noexcept -> char const* {
+    return cBinaryTestErrorCategoryName.data();
+}
+
+
+YSTDLIB_ERROR_HANDLING_MARK_AS_ERROR_CODE_ENUM(BinaryErrorCodeEnum);
 
 int main() {
     // Test error codes within the same error category
     BinaryErrorCode const success{BinaryErrorCodeEnum::Success};
-    std::error_code const success_error_code{success};
-    assert((success == success_error_code));
-    assert((cSuccessErrorMsg == success_error_code.message()));
-    assert((BinaryErrorCode::get_category() == success_error_code.category()));
-    assert((cBinaryTestErrorCategoryName == success_error_code.category().name()));
 
     BinaryErrorCode const failure{BinaryErrorCodeEnum::Failure};
-    std::error_code const failure_error_code{failure};
-    assert((failure == failure_error_code));
-    assert((cFailureErrorMsg == failure_error_code.message()));
-    assert((BinaryErrorCode::get_category() == failure_error_code.category()));
-    assert((cBinaryTestErrorCategoryName == failure_error_code.category().name()));
-
-    assert((success_error_code != failure_error_code));
-    assert((success_error_code.category() == failure_error_code.category()));
-
-    AlwaysSuccessErrorCode const always_success{AlwaysSuccessErrorCodeEnum::Success};
-    std::error_code const always_success_error_code{always_success};
-    assert((always_success_error_code == always_success));
-    assert((cSuccessErrorMsg == always_success_error_code.message()));
-    assert((AlwaysSuccessErrorCode::get_category() == always_success_error_code.category()));
-    assert((cAlwaysSuccessErrorCategoryName == always_success_error_code.category().name()));
-
-    // Compare error codes from different error category
-    // Error codes that have the same value or message won't be the same with each other if they are
-    // from different error categories.
-    assert((success_error_code.value() == always_success_error_code.value()));
-    assert((success_error_code.message() == always_success_error_code.message()));
-    assert((success_error_code.category() != always_success_error_code.category()));
-    assert((success_error_code != always_success_error_code));
-    assert((AlwaysSuccessErrorCode{AlwaysSuccessErrorCodeEnum::Success} != success_error_code));
-    assert((BinaryErrorCode{BinaryErrorCodeEnum::Success} != always_success_error_code));
 
     std::cout << "test is good!" << std::endl;
     return 0;
